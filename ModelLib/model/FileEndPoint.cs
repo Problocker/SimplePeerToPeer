a4d3dccee17c502fa7e2fc8b72b47b7df2ef691a
Problocker <55052514+Problocker@users.noreply.github.com>
2020-10-20 09:39:18 +0200

    Lavet ModelKlasse

---
+++ ModelLib/model/FileEndPoint.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModelLib.model
+{
+    public class FileEndPoint
+    {
+        private string _ipAddress;
+        private int _port;
+
+        public FileEndPoint()
+        {
+        }
+
+        public FileEndPoint(string ipAddress, int port)
+        {
+            _ipAddress = ipAddress;
+            _port = port;
+        }
+
+        public string IpAddress
+        {
+            get => _ipAddress;
+            set => _ipAddress = value;
+        }
+
+        public int Port
+        {
+            get => _port;
+            set => _port = value;
+        }
+    }
+}
